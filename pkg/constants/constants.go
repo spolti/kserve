0e8bd06b42fdf181859b95e8c2c5b7486d713531
Filippe Spolti <fspolti@redhat.com>
2025-04-29 14:43:23 -0300

    Prevent External Route Changes To ISVCs From Creating New Revisions (#549) (#578)
    * adding networking.knative.io/visibility annotation to ServiceAnnotationDisallowedList
    
    
    
    * adding comments and fixing python linter error
    
    
    
    * add unit test
    
    
    
    * correcting annotation name
    
    
    
    * removing visibility annotation from the ServiceAnnotationDisallowedList constant as it is not needed for backward compatibility
    
    
    
    * fix unit test
    
    
    
    * improving unit test structure
    
    
    
    * removing visibilityAnnotation constant
    
    
    
    ---------
    
    Signed-off-by: Brett Thompson <196701379+brettmthompson@users.noreply.github.com>
    Co-authored-by: Brett Thompson <196701379+brettmthompson@users.noreply.github.com>

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -370,6 +370,9 @@ const (
 )
 
 var (
+	// ServiceAnnotationDisallowedList is a list of annotations that are not allowed to be propagated to Knative
+	// revisions, which prevents the reconciliation loop to be triggered if the annotations is
+	// configured here are used.
 	ServiceAnnotationDisallowedList = []string{
 		autoscaling.MinScaleAnnotationKey,
 		autoscaling.MaxScaleAnnotationKey,
@@ -377,7 +380,8 @@ var (
 		"kubectl.kubernetes.io/last-applied-configuration",
 		"security.opendatahub.io/enable-auth",
 	}
-
+	// RevisionTemplateLabelDisallowedList is a list of labels that are not allowed to be propagated to Knative
+	// revisions, which prevents the reconciliation loop to be triggered if the labels is configured here are used.
 	RevisionTemplateLabelDisallowedList = []string{
 		VisibilityLabel,
 	}
