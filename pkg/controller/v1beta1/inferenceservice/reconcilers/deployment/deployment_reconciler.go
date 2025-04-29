3061f2778da09232bd32f50e2af1f95a3289b473
Filippe Spolti <fspolti@redhat.com>
2025-04-29 13:46:04 -0300

    disable autoMountServiceAccountToken: true globally  (#553) (#576)
    * disable autoMountServiceAccountToken globally and only enable when raw auth is requested
    
    
    
    * pr feedback
    
    
    
    ---------
    
    Signed-off-by: Vedant Mahabaleshwarkar <vmahabal@redhat.com>
    Co-authored-by: Vedant Mahabaleshwarkar <vmahabal@redhat.com>

--- pkg/controller/v1beta1/inferenceservice/reconcilers/deployment/deployment_reconciler.go
+++ pkg/controller/v1beta1/inferenceservice/reconcilers/deployment/deployment_reconciler.go
@@ -25,6 +25,7 @@ import (
 	"strconv"
 	"strings"
 
+	"google.golang.org/protobuf/proto"
 	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/client-go/kubernetes"
 
@@ -262,6 +263,8 @@ func addOauthContainerToDeployment(clientset kubernetes.Interface, deployment *a
 		}
 		updatedPodSpec := deployment.Spec.Template.Spec.DeepCopy()
 		//	updatedPodSpec := podSpec.DeepCopy()
+		// ODH override. See : https://issues.redhat.com/browse/RHOAIENG-19904
+		updatedPodSpec.AutomountServiceAccountToken = proto.Bool(true)
 		updatedPodSpec.Containers = append(updatedPodSpec.Containers, *oauthProxyContainer)
 		deployment.Spec.Template.Spec = *updatedPodSpec
 	}
