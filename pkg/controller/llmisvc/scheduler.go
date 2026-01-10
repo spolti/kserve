28b5359cb533d4af69a2511aaf839cf716944a62
Pierangelo Di Pilato <pierdipi@redhat.com>
2026-01-10 03:12:04 +0100

    Add zmq port to scheduler service for precise prefix cache routing (#1047)
    Add 5557 as the (default) port for the zmq enpoint for kv-events.
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- pkg/controller/llmisvc/scheduler.go
+++ pkg/controller/llmisvc/scheduler.go
@@ -205,7 +205,7 @@ func (r *LLMInferenceServiceReconciler) expectedSchedulerService(ctx context.Con
 	if llmSvc.Spec.Router != nil && llmSvc.Spec.Router.Scheduler != nil && llmSvc.Spec.Router.Scheduler.Template != nil {
 		podSpec := llmSvc.Spec.Router.Scheduler.Template.DeepCopy()
 
-		desiredPorts := sets.New("grpc", "grpc-health", "metrics")
+		desiredPorts := sets.New("grpc", "grpc-health", "metrics", "zmq")
 
 		actualPorts := make(map[string]*corev1.ContainerPort)
 		for _, container := range podSpec.Containers {
